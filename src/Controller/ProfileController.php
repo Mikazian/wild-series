f1474869fcfe9449122dc97f4c0cc1b88035a9eb
Mike Xiong <xiong.tchylon@gmail.com>
2023-06-10 20:25:47 +0200

    add profile in navbar

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/profile', name: 'profile_')]
 class ProfileController extends AbstractController
 {
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         // usually you'll want to make sure the user is authenticated first,
